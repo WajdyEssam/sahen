f5ab446aaaac33d23543ff02b7bdbc6cd7ca5c17
Wajdy Essam <wajdyessam@ksu.edu.sa>
2013-08-31 21:38:06 +0300

    add action bar on tab activity

--- src/com/malaz/reports/ReportsMainActivity.java
+++ src/com/malaz/reports/ReportsMainActivity.java
@@ -1,11 +1,22 @@
 package com.malaz.reports;
 
 import com.malaz.app.BaseActivity;
+import com.malaz.app.HistoryActivity;
+import com.malaz.app.MainActivity;
 import com.malaz.app.R;
+import com.malaz.util.AppUtil;
+import com.malaz.util.Constants;
+import com.malaz.util.Preferences;
 
+import android.app.ActionBar;
 import android.app.TabActivity;
 import android.content.Intent;
+import android.content.res.Resources;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.widget.TabHost;
 
 public class ReportsMainActivity extends TabActivity {
@@ -14,6 +25,7 @@ public class ReportsMainActivity extends TabActivity {
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_tab_reports);
+		initializingActionBar();
 		
 		TabHost tabHost = getTabHost();
 
@@ -36,5 +48,48 @@ public class ReportsMainActivity extends TabActivity {
 				.setContent(new Intent(this, YearlyReportActivity.class)));
 		
 		tabHost.setCurrentTab(0);
+	}
+	
+	// repeated code from base activity, until we found away to inhert this code
+	
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		MenuInflater inflater = getMenuInflater();
+		inflater.inflate(R.menu.activity_base, menu);
+		return true;
+	}
+	
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		switch (item.getItemId()) {
+		case (android.R.id.home):
+			Intent intent = new Intent(this, MainActivity.class);
+			intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+			startActivity(intent);
+			break;
+			
+		case R.id.history:
+			Intent intent2 = new Intent(this, HistoryActivity.class);
+			startActivity(intent2);
+			break;			
+		}
+		
+		return super.onOptionsItemSelected(item);
+	} 
+	
+	protected void initializingActionBar() {
+		ActionBar actionBar = getActionBar();
+		actionBar.show();
+		
+		Resources resources = getResources();
+		Drawable drawable = resources.getDrawable(R.drawable.header);
+		actionBar.setBackgroundDrawable(drawable);
+		
+		actionBar.setDisplayUseLogoEnabled(false);
+		actionBar.setHomeButtonEnabled(true);
+		actionBar.setTitle(this.getResources().getString(R.string.app_name));
+		actionBar.setSubtitle(this.getResources().getString(R.string.sim_name) + 
+				" : " +  AppUtil.getSIMName(Preferences.getValue(this, Constants.COMPANY_NAME, "")));
+		
 	}
 }
--- src/com/malaz/util/Constants.java
+++ src/com/malaz/util/Constants.java
@@ -5,10 +5,10 @@ public class Constants {
 	public final static String SENDING_BALANCE_OPERATION = "2";
 	public final static String SENDING_CALLME_OPERATION  = "3";
 	
-	public final static String CHARGING_OPERATION_ARABIC_MSG = "تم شحن الرصيد";
+	public final static String CHARGING_OPERATION_ARABIC_MSG = "تم شحن الرصيد  ";
 	public final static String CHARGING_OPERATION_ENGLISH_MSG = "charging balance";
 	
-	public final static String SENDING_BALANCE_OPERATION_ARABIC_MSG = "تم تحويل الرصيد";
+	public final static String SENDING_BALANCE_OPERATION_ARABIC_MSG = "تم تحويل الرصيد  ";
 	public final static String SENDING_BALANCE_OPERATION_ENGLISH_MSG = "sending balance";
 	
 	public final static String SENDING_CALLME_OPERATION_ARABIC_MSG = "تم ارسال رسالة كلمني";
